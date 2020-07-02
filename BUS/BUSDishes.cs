112a7e15ebf72d408032bafc10d83f9c013b75cd
Hòa Lưu Đức <19424022@student.hcmus.edu.vn>
2020-07-03 00:45:29 +0700

    Add dishes and menu

---
+++ BUS/BUSDishes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAO;
+
+namespace BUS
+{
+    public class BUSDishes
+    {
+        private static BUSDishes instance;
+
+        public static BUSDishes Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BUSDishes();
+                }
+                return instance;
+            }
+        }
+
+        public void ShowDishes(DataGridView dataGridView)
+        {
+            dataGridView.DataSource = DAODishes.Instance.Show();
+        }
+
+        public Boolean Insert_Food(FOOD food, List<FOOD_RESOURCE> fOODs)
+        {
+            List<FOOD_RESOURCE> temp = new List<FOOD_RESOURCE>(fOODs);
+            temp.RemoveAt(temp.Count - 1);
+            return DAODishes.Instance.Insert_Food(food, temp);
+        }
+
+        public List<FOOD_RESOURCE> FoodResource()
+        {
+            return DAODishes.Instance.FoodResource();
+        }
+    }
+}
---
+++ Demo/DISHES_UI/Dishes.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Demo.DISHES_UI
+{
+    partial class Dishes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dbGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dbGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(296, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH SÁCH MÓN ĂN";
+            // 
+            // dbGridView
+            // 
+            this.dbGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbGridView.Location = new System.Drawing.Point(12, 113);
+            this.dbGridView.Name = "dbGridView";
+            this.dbGridView.Size = new System.Drawing.Size(643, 328);
+            this.dbGridView.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 104);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(661, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 104);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(661, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 104);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Dishes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dbGridView);
+            this.Controls.Add(this.label1);
+            this.Name = "Dishes";
+            this.Text = "Dishes";
+            this.Load += new System.EventHandler(this.Dishes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dbGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dbGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
